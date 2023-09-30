9611539443703c9dfecefc8e3f58bb5fdec1c259
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-30 10:18:21 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4,5,6   ,7};
+            Console.WriteLine($"Max:{Print(ints).max} \n Min:{Print(ints).min}");
+        }
+
+        static (int max, int min) Print(int[] arr)
+        {// maxsimal element qiymati min ga min element qiymati maxga teng: 
+            (int max, int min) value = (int.MinValue, int.MaxValue);
+
+            foreach(int item in arr)
+            {
+                if (value.max < item)
+                {
+                    value.max = item;
+                }
+                if(value.min > item)
+                {
+                    value.min = item;
+                }
+            }
+            return value;
+
+
+        }
+
+    }
+}
